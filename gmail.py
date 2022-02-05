d55ca456976e8d96099c9d148e9767be79c521be
insaiyancvk <cvamshik1@gmail.com>
2022-02-06 02:13:06 +0530

    experimenting with gmail api

--- gmail.py
+++ gmail.py
@@ -7,7 +7,6 @@ from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
-# from googleapiclient.errors import HttpError
 
 SCOPES = [
     'https://www.googleapis.com/auth/gmail.readonly',
@@ -20,19 +19,23 @@ SCOPES = [
 creds = None
 
 if os.path.exists('token.json'):
-    creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    creds = Credentials.from_authorized_user_file('templates/token.json', SCOPES)
 
+
+# print(help(creds))
 if not creds or not creds.valid:
     if creds and creds.expired and creds.refresh_token:
         creds.refresh(Request())
     else:
         flow = InstalledAppFlow.from_client_secrets_file(
             'credentials.json', SCOPES)
+
         creds = flow.run_local_server(port=0)
         
-    with open('token.json', 'w') as token:
+    with open('templates/token.json', 'w') as token:
         token.write(creds.to_json())
-
+        
 spam = {}
 
 with open('spam.json','r') as f:
@@ -41,30 +44,6 @@ with open('spam.json','r') as f:
 id = spam['drafts'][0]['id']
 
 service = build('gmail', 'v1', credentials=creds)
-
-
-# results = service.users().drafts().list(userId='me').execute()
-# print(json.dumps(results, indent=3))
-
-# results = service.users().drafts().send(
-#     userId='me',
-#     body = {'id': ''}
-#     ).execute()
-# print(results)
-# labels = results.get('labels', [])
-# for i in results['drafts']:
-#     print(i)
-
-    # if not labels:
-    #     print('No labels found.')
-    # print('Labels:')
-    # for label in labels:
-    #     print(label['name'])
-
-# except HttpError as error:
-#     # TODO(developer) - Handle errors from gmail API.
-#     print(f'An error occurred: {error}')
-
 
 def json_pretty_print(obj):
   print(json.dumps(obj,indent=3))
@@ -95,3 +74,5 @@ def list_drafts():
     """
     return service.users().drafts().list(
         userId='me').execute()
+
+# json_pretty_print(list_drafts())
