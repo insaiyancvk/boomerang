73cac2e14c5d682e7a7d96f9bbb772529799cc3b
insaiyancvk <cvamshik1@gmail.com>
2022-02-06 02:12:38 +0530

    return formatted time and remove unused variables

--- datepicker.py
+++ datepicker.py
@@ -12,77 +12,89 @@ def all_children (window) :
 
     return _list
 
+def picker():
 
-root = Tk()
-d = date.today()
- 
-# Set geometry
-root.geometry("270x700")
- 
-selDate = ''
-# Add Calendar
-cal = Calendar(
-            root, 
-            mindate = date.today(),
-            background = 'Black',
-            selectmode = 'day',
-            firstweekday = 'sunday',
-            selectbackground = 'Black',
-            selectforeground = "White",
-            weekendbackground = 'Red',
-            weekendforeground = 'White',
-            showweeknumbers = False,
-            normalbackground = 'White',
-            normalforeground = 'Black',
-            othermonthforeground = 'Black',
-            othermonthbackground = "White",
-            othermonthweforeground = '#949494',
-            othermonthwebackground = '#EBECF0',
-            # textvariable = selDate,
-            date_pattern = 'd/m/y',
-            year = d.year,
-            month = d.month,
-            day = d.day)
- 
+    root = Tk()
+    d = date.today()
+    
+    # Set geometry
+    root.geometry("270x700")
+    
+    # Add Calendar
+    cal = Calendar(
+                root, 
+                mindate = date.today(),
+                background = 'Black',
+                selectmode = 'day',
+                firstweekday = 'sunday',
+                selectbackground = 'Black',
+                selectforeground = "White",
+                weekendbackground = 'Red',
+                weekendforeground = 'White',
+                showweeknumbers = False,
+                normalbackground = 'White',
+                normalforeground = 'Black',
+                othermonthforeground = 'Black',
+                othermonthbackground = "White",
+                othermonthweforeground = '#949494',
+                othermonthwebackground = '#EBECF0',
+                date_pattern = 'd/m/y',
+                year = d.year,
+                month = d.month,
+                day = d.day)
+    
 
-cal.pack(pady = 20)
+    cal.pack(pady = 20)
 
-time_picker = AnalogPicker(root)
-time_picker.pack(expand = True, fill="both")
+    time_picker = AnalogPicker(root)
+    time_picker.pack(expand = True, fill="both")
 
-theme = AnalogThemes(time_picker)
-theme.setNavyBlue()
+    theme = AnalogThemes(time_picker)
+    theme.setNavyBlue()
 
-Button(
-    root, 
-    text = "Done",
-    command = root.destroy).pack(pady = 20)
+    Button(
+        root, 
+        text = "Done",
+        command = root.destroy).pack(pady = 20)
 
-ldate = Label(root, text = "")
-ldate.pack(pady = 5)
-timelbl = Label(root, text = "")
-timelbl.pack(pady = 5)
+    ldate = Label(root, text = "")
+    ldate.pack(pady = 5)
+    timelbl = Label(root, text = "")
+    timelbl.pack(pady = 5)
 
-root.mainloop()
+    root.mainloop()
+    
+    return [int(i) if i.isdigit() else str(i) for i in cal.get_date().split('/')], list(time_picker.time())
+
+def formatted_datetime():
+
+    sdate, stime = picker()
+    # goal - YYYY-MM-DDTHH:MM ✅
+
+    t = str(stime[0])+":"+str(stime[1])+" "+stime[2] 
+    in_time = datetime.strptime(t, "%I:%M %p")
+    out_time = datetime.strftime(in_time, "%H:%M")
+    return str(sdate[2])+"-"+str(sdate[1])+"-"+str(sdate[0])+"T"+out_time
+
 
 # DD/MM/YYYY
-sdate, stime = cal.get_date(), list(time_picker.time())
+# sdate, stime = picker()
 # split the date string by '/' and convert the applicable strings to integer and leave the rest alone
-sdate = [int(i) if i.isdigit() else str(i) for i in sdate.split('/')]
+# sdate = 
 
 # Current time
-def current_time():
-    t = datetime.now().strftime("%H %M %p")
-    t = t.split()
-    return [int(i) if i.isdigit() else str(i) for i in t]   # [int, int, str] - [hh, mm, AM/PM]
+# def current_time():
+#     t = datetime.now().strftime("%H %M %p")
+#     t = t.split()
+#     return [int(i) if i.isdigit() else str(i) for i in t]   # [int, int, str] - [hh, mm, AM/PM]
     
 # Compare selected time with current time
-print(stime, current_time(), stime == current_time())
+# print(stime, current_time(), stime == current_time())
 
 # Current date
-def current_date():
-    dat = date()
-    return [dat.day, dat.month, dat.year]   # [int, int, int] - [dd, mm, yyyy] # TO BE USED: YYYY-MM-DD #
-today = [d.day, d.month, d.year]
+# def current_date():
+#     dat = date()
+#     return [dat.day, dat.month, dat.year]   # [int, int, int] - [dd, mm, yyyy] # TO BE USED: YYYY-MM-DD #
+# today = [d.day, d.month, d.year]
 # Compare selected date with current date  
-print(today, sdate, sdate == today)
+# print(today, sdate, sdate == today)
